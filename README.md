6303e6b47952951716cb0198958ab654fcce3cb2
Francisco José Díaz <74576885+Fridnansen@users.noreply.github.com>
2022-09-26 13:11:40 -0300

    CRUD
    prueba

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ CRUD with javascript, nodejs, and mysql 8. List of tasks with javascript, where 
 CRUD con javascript, nodejs, y mysql 8. Lista de tareas con javascript, donde podemos añadir tareas, visualizarlas en una tabla, editar las tareas y eliminarlas. Es una aplicación sencilla donde podemos aprender los conceptos básicos para realizar aplicaciones más complejas con javascript.
 
 CRUD avec javascript, nodejs, et mysql 8. liste de tâches avec javascript, où nous pouvons ajouter des tâches, les visualiser dans un tableau, éditer les tâches et les supprimer. C'est une application simple où nous pouvons apprendre les concepts de base pour faire des applications plus complexes avec javascript.
+
+### is Readme MK
--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const myconnection = require('express-myconnection');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const tasksRoutes = require('./routes/tasks');
 
 const app = express();
@@ -22,13 +22,13 @@ app.set('view engine', 'hbs');
 app.use(myconnection(mysql, {
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: '12345',
   port: 3306,
   database: 'crudnodejs'
 }, 'single'));
 
 app.listen(app.get('port'), () => {
-  console.log('Listening on port ', app.get('port'));
+  console.log('Este es el puerto ', app.get('port'));
 });
 
 app.use('/', tasksRoutes);
