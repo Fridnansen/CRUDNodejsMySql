4f0d4bab6f1e0b475b54c6af66db8032dfc3930c
Francisco José Díaz <74576885+Fridnansen@users.noreply.github.com>
2022-09-28 10:33:36 -0300

    Updates

---
+++ peliculas.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM crudnodejs.tasks
+LIMIT 0, 1000
+
+-- Date: 2022-09-27 14:46
+*/
+INSERT INTO `` (`id`,`title`,`description`) VALUES (1,'Titanic','Romantica protagonizada por Leonardo Dicaprio');
+INSERT INTO `` (`id`,`title`,`description`) VALUES (3,'Terminator','Ciencia Ficción protagonizada por Arnold Schwarzenegger');
